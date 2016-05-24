154fe96a411c886f05845cf57cbeb3f6de0d0aba
Eric Yu <ericyu@gmail.com>
2016-05-24 23:09:20 +0800

    Add new program to chain timetables

---
+++ ChainTimeTableTrains.py
@@ -0,0 +1,164 @@
+# -*- coding: utf8 -*-
+import json
+from os.path import join
+from pathlib import Path
+from bisect import bisect
+from collections import namedtuple
+import Util
+
+# configuration
+inputDir = 'output/Stations'
+outputDir = 'output/Lines'
+
+
+lineCodes = ['R', 'G', 'O', 'BL'] 
+# 0: 小到大
+directionMapping = {
+    '往R22北投站、R28淡水站': ('淡水', 0),
+    '往R28淡水站': ('淡水', 0),
+    '往R05大安站、R02象山站': ('象山', 1),
+    '往R02象山站': ('象山', 1),
+    
+    '往G19松山站': ('松山', 0),
+    '往G08台電大樓站、G01新店站': ('新店', 1),
+    '往G01新店站': ('新店', 1),
+    
+    '往O54蘆洲站': ('蘆洲、迴龍', 0),
+    '往O54蘆洲站、O21迴龍站': ('蘆洲、迴龍', 0),
+    '往O21迴龍站': ('蘆洲、迴龍', 0),
+    '往O01南勢角站': ('南勢角', 1),
+    
+    '往BL23南港展覽館站': ('南港展覽館', 0),
+    '往BL05亞東醫院站、BL01頂埔站': ('頂埔', 1),
+    '往BL01頂埔站': ('頂埔', 1)
+}
+
+lastAppend = {
+    'R27': ('R28', 3), # 淡水
+    'R03': ('R02', 2), # 象山
+    'R21': ('R22', 2), # 北投
+    'R06': ('R05', 2), # 大安
+
+    'BL22': ('BL23', 2), # 南港展覽館
+    'BL02': ('BL01', 3), # 頂埔 
+    'BL20': ('BL21', 2), # 昆陽 
+    'BL06': ('BL05', 3), # 亞東醫院
+
+    'G18': ('G19', 3), # 松山 
+    'G02': ('G01', 2), # 新店
+    'G09': ('G08', 2), # 台電大樓
+
+    'O02': ('O01', 2), # 南勢角
+    'O53': ('O54', 2), # 蘆洲 
+    'O20': ('O21', 3)  # 迴龍 
+}
+
+additionalTimeThreshold = {
+    ('O05', 'O04'): 2
+}
+
+SingleTrainStruct = namedtuple('SingleTrainStruct', ['Dst', 'Schedule'])
+
+def ChainTimeTables(direction, timetables):
+    orderedStationList = sorted(timetables.keys())
+    if direction[1]:
+        orderedStationList.reverse()
+
+    # 找出所有的終點站，並將其分離
+    allDsts = set()
+    for timetable in timetables.values():
+        allDsts.update(map(lambda k: k['Dst'], timetable))
+
+    # 按照目的地分離，加速搜尋
+    for code, timetable in timetables.items():
+        newTimetable = {}
+        for dst in allDsts:
+            newTimetable[dst] = []
+        for d in timetable:
+            # 同時先將時間都轉為數字，方便搜尋
+            newTimetable[d['Dst']].append(Util.ConvertToMinute(d['Time']))
+        # 以防萬一，還是先按照時間排序一次
+        for schedule in newTimetable.values():
+            schedule.sort()
+        # 替換原先的時刻表
+        timetables[code] = newTimetable
+    if direction[0] == '南勢角' and direction[1] == 1:
+        # 特別處理
+        list1 = [code for code in orderedStationList if code <= 'O21'] # 迴龍
+        list2 = [code for code in orderedStationList if code <= 'O12' or code >= 'O50'] # 蘆洲
+        result1 = TraverseTimeTables(list1, timetables, specialHandle=1)
+        result2 = TraverseTimeTables(list2, timetables)
+        result = result1 + result2
+    else:
+        result = TraverseTimeTables(orderedStationList, timetables)
+    return result
+    
+def appendLastStation(schedule):
+    theLast = schedule[-1]
+    (dstStation, additionMins) = lastAppend[theLast['StationCode']]
+    schedule.append({'StationCode': dstStation, 'DepTime': Util.ConvertToHourMinute(Util.ConvertToMinute(theLast['DepTime']) + additionMins)})
+    
+def TraverseTimeTables(orderedStationList, timetables, specialHandle=0):    
+    # 從第一個站開始，看第一筆時刻，然後接著每一個站去找
+    result = []
+    for idx, stationCode in enumerate(orderedStationList):
+        if specialHandle == 1 and stationCode <= 'O12':
+            continue
+        for dst, departures in timetables[stationCode].items():
+            for d in departures:
+                singleTrain = SingleTrainStruct(Dst=dst, Schedule=[])
+                singleTrain.Schedule.append({'StationCode': stationCode, 'DepTime': Util.ConvertToHourMinute(d)})
+                timeThreshold = d
+                for j in range(idx+1, len(orderedStationList)):
+                    currentTimetable = timetables[orderedStationList[j]][dst]
+                    if not currentTimetable:
+                        continue
+                    # 找到下一個時間
+                    timeThreshold += additionalTimeThreshold.get((orderedStationList[j-1], orderedStationList[j]), 0)
+                    foundIdx = bisect(currentTimetable, timeThreshold)
+                    foundTime = currentTimetable[foundIdx]
+                    singleTrain.Schedule.append({'StationCode': orderedStationList[j], 'DepTime': Util.ConvertToHourMinute(foundTime)})
+                    timeThreshold = foundTime
+                    del currentTimetable[foundIdx]
+                # 然後 append 最後一個站
+                appendLastStation(singleTrain.Schedule)
+                result.append({'Dst': singleTrain.Dst, 'Schedule': singleTrain.Schedule})
+            del departures[:]
+    return result
+
+def ProcessLines(lineCode, stationFiles):
+    # 讀進所有的時刻表資料
+    timetableList = list(map(Util.ReadJson, stationFiles))
+    # 先收集這個時刻表的編排是按照星期幾的，以及方向
+    timetables = {}
+    effectiveFromSet = set()
+    for station in timetableList:
+        for timetable in station['Timetables']:
+            direction = directionMapping[timetable['Direction']]
+            effectiveFromSet.add(timetable['EffectiveFrom'])
+            for schedule in timetable['Schedule']:
+                day = schedule['Days']
+                key = (direction, day)
+                if key not in timetables.keys():
+                    timetables[key] = {}
+                timetables[key][station['StationCode']] = schedule['Departures']
+    directions = sorted(set([x[0] for x in timetables.keys()]))
+    days = sorted(set([x[1] for x in timetables.keys()]))
+    assert(len(effectiveFromSet) == 1)
+    effectiveFrom = effectiveFromSet.pop()
+
+    # 現在得到的是依照方向及日期區分的時刻表 timetables
+    result = []
+    for direction in directions:
+        directionResult = []
+        for day in days:
+#            if day != '7':
+#                continue
+            directionResult.append({ 'Days': day, 'Trains': ChainTimeTables(direction, timetables[(direction, day)]) })
+        result.append({'Direction': direction[0], 'EffectiveFrom': effectiveFrom, 'Timetables': directionResult})
+    with open(join('output/Lines', lineCode + '.json'), 'w', encoding='utf-8') as f:
+        json.dump(result, f, ensure_ascii=False, sort_keys=True, indent=2)
+
+if __name__ == '__main__':
+    for lineCode in lineCodes:
+        ProcessLines(lineCode, Path(inputDir).glob(lineCode + '*.json'))
---
+++ Util.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8 -*-
+import json
+import re
+
+def ConvertToMinute(time):
+    hour = int(time[:2])
+    minute = int(time[-2:])
+    if hour < 2:
+        hour += 24
+    return hour * 60 + minute
+
+def ConvertToHourMinute(minute):
+    hour = minute // 60
+    minute = minute - hour*60
+    if hour >= 24:
+        hour -= 24
+    return '{0:02d}:{1:02d}'.format(hour, minute)
+
+def ReadJson(file):
+    with file.open(encoding='UTF-8') as f:
+        return json.load(f)
+
+def GetNumber(string):
+    return int(re.findall('\d+', string)[0])
+
+class StationMapping:
+    codeToName = {}
+    nameToCode = {}
+    
+    def __init__(self, fileName):
+        with open(fileName, 'r', encoding='utf-8') as f:
+            stationData = json.load(f)
+        for station in stationData:
+            self.codeToName[station['Code']] = station['Name']
+            self.nameToCode[station['Name']] = station['Code']
+    
+    def CodeToName(self, code):
+        return self.codeToName[code]
+
+    def NameToCode(self, name):
+        return self.nameToCode[name]
